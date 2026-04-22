491a54a510fce1e05369b71dda7d422e9c882313
URangelPalacios <u_rangel@outlook.com>
2026-04-22 15:53:38 -0600

    cronómetro funcional con detección de actividad

---
+++ script.js
@@ -0,0 +1,35 @@
+let tiempo = 0;
+let activo = false;
+let intervalo = null;
+let timeoutInactividad = null;
+
+const tiempoElemento = document.getElementById("tiempo");
+const estadoElemento = document.getElementById("estado");
+
+function iniciarCronometro() {
+    intervalo = setInterval(() => {
+        if(activo){
+            tiempo++;
+            tiempoElemento.textContent = tiempo;
+        }
+    }, 1000);
+    
+}
+
+function actividadDetectada() {
+    activo = true;
+    estadoElemento.textContent = "Activo";
+
+    clearTimeout(timeoutInactividad);
+
+    timeoutInactividad = setTimeout(() => {
+        activo = false;
+        estadoElemento.textContent = "Inactivo";
+}, 10000)
+    
+}
+
+document.addEventListener("mousemove", actividadDetectada);
+document.addEventListener("keydown", actividadDetectada);
+
+iniciarCronometro();
